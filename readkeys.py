5a2bfe01d7c4d618deabbaaa8fb85fefd7f9dfd7
azaroma <azaro.magio@gmail.com>
2016-12-04 15:41:26 -0600

    Read keys from .ini file

---
+++ readkeys.py
@@ -0,0 +1,17 @@
+from configparser import ConfigParser
+
+def readKeys(filename='keys.ini', section='keys'):
+    """ Read Twitter API keys """
+
+    parser = ConfigParser()
+    parser.read(filename)
+
+    keys = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            keys[item[0]] = item[1]
+    else:
+        raise Exception('{} not found in the {} file'.format(section, filename))
+
+    return keys
---
+++ twitter_streaming.py
@@ -0,0 +1,27 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from readkeys import readKeys
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    keys = readKeys()
+
+    # This handles Twitter authetification and the connection to Twitter Streaming API
+    listener = StdOutListener()
+    auth = OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
+    auth.set_access_token(keys['access_token'], keys['access_token_secret'])
+    stream = Stream(auth, listener)
+
+    # This line filter Twitter Streams to capture data by keywords
+    stream.filter(languages=["es"], track=['puta'])
