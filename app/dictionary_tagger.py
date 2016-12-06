5f624b26edfed2c6da4bf3a1535d8cbb2e9cc522
azaroma <azaro.magio@gmail.com>
2016-12-05 23:33:58 -0600

    Sexist tagger ready to be plugged

---
+++ app/dictionary_tagger.py
@@ -0,0 +1,30 @@
+import yaml
+
+class DictionaryTagger():
+
+    def __init__(self, dictionary_paths):
+        files = [open(path, "r") for path in dictionary_paths]
+        dictionaries = [yaml.load(dict_file) for dict_file in files]
+        (file.close() for file in files)
+        self.dictionary = {}
+        for curr_dict in dictionaries:
+            for key in curr_dict:
+                if key in self.dictionary:
+                    self.dictionary[key].extend(curr_dict[key])
+                else:
+                    self.dictionary[key] = curr_dict[key]
+
+        def tag(self, postagged_sentences):
+            return [self.tag_sentence(sent) for sent in postagged_sentences]
+
+        def tag_sentence(self, sentence, tag_with_lemmas=None):
+            tagged_sentence = []
+            for word in sentence:
+                expression_form = word[0]
+                literal = expression_form
+                if literal in self.dictionary:
+                    tags = [tag for tag in self.dictionary[literal]]
+                    tagged_expression = (expression_form, tags)
+                    tagged_sentence.append(tagged_expression)
+
+            return tagged_sentence
