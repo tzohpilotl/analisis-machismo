5bff3c3bda7d316c49e9d3a8c5e7cdf63ef991aa
azaroma <azaro.magio@gmail.com>
2016-12-07 15:15:39 -0600

    Add 'stop after # tweets retrieved' functionality to listener

--- app/twitter_miner.py
+++ app/twitter_miner.py
@@ -5,8 +5,10 @@ from key_reader import KeyReader
 
 class _StdOutListener(StreamListener):
 
-    def __init__(self, output_filename=None):
+    def __init__(self, max_tweets, output_filename=None):
         super(_StdOutListener, self).__init__()
+        self.mined_tweets = 0
+        self.max_tweets = max_tweets
         try:
             self.output_file = open(output_filename, "w")
         except TypeError as err:
@@ -17,22 +19,31 @@ class _StdOutListener(StreamListener):
             print(data)
         else:
             self.output_file.write(data)
-        return True
+        self.mined_tweets += 1
+        try:
+            if self.mined_tweets >= self.max_tweets:
+                raise RuntimeError
+        except RuntimeError:
+            print("Mined {} tweets".format(self.mined_tweets))
+            return False
+        else:
+            return True
 
     def on_error(self, status):
         print(status)
 
 class TwitterMiner():
 
-    def __init__(self, keys_file='keys.ini', output_file=None):
+    def __init__(self, keys_file='keys.ini', output_file=None, max_tweets=10):
         self.keys_file = keys_file
         self.output_file = output_file
+        self.max_tweets = max_tweets
 
     def connect(self):
         self.key_reader = KeyReader()
         self.keys = self.key_reader.read('keys.ini', 'keys')
 
-        listener = _StdOutListener(self.output_file)
+        listener = _StdOutListener(self.max_tweets, self.output_file)
         auth = OAuthHandler(self.keys['consumer_key'], self.keys['consumer_secret'])
         auth.set_access_token(self.keys['access_token'], self.keys['access_token_secret'])
         self.stream = Stream(auth, listener)
