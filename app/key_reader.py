8a060bfa4afb0c674e50f76bfad56f6f37852a7a
comrade001 <abrahamsa@live.com.mx>
2016-12-07 17:35:43 -0600

    Add docstring to function read in KeyReader class

--- app/key_reader.py
+++ app/key_reader.py
@@ -1,13 +1,16 @@
 from configparser import ConfigParser
+from pydoc
 
 class KeyReader():
+""" Keyreader to load keys.ini file """
 
     def __init__(self):
         self.parser = ConfigParser()
         self.keys = {}
-
+    
     def read(self, filename='keys.ini', section='keys'):
-        self.keys.clear()
+    """ Import the configparser, tell it to read the file, and get a listing of the sections. Sections are listed in square brackets [] """	
+	self.keys.clear()
         self.parser.read(filename)
 
         if self.parser.has_section(section):
