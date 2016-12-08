5af17433a8ce613dff69fcaa6b354cc8cfda562f
azaroma <azaro.magio@gmail.com>
2016-12-08 00:41:51 -0600

    Rewrite tag_counter documentation

--- app/tag_counter.py
+++ app/tag_counter.py
@@ -1,10 +1,17 @@
 class TagCounter():
-
+    """ Wrapper for a tag counting method """
+    
     def __init__(self, tagged_tweets):
+        """ Store a list of tweets in case none is provided in count() """
+        
         self.tags = {}
         self.tagged_tweets = tagged_tweets
 
     def count(self, tagged_tweets=None):
+        """Handle the counting of tags inserting them in a dictionary to 
+        ensure their uniqueness. Can manage a list of tags and a single
+        string tag.
+        """
         if tagged_tweets is not None:
             self.tagged_tweets = tagged_tweets
         for tweet in self.tagged_tweets:
